1e7b9fc7e2ba022c176bf7a894135c7cc37c416b
Lars Isenberg <isenberg.lars@gmail.com>
2021-04-05 15:38:53 +0200

    New todos

--- TODOS.R
+++ TODOS.R
@@ -1,2 +1,4 @@
+# TODO: If possible make functions error if the file is definitely not of the correct type
 # TODO: Check all read functions and remove unrelated functionality (renaming etc.)
+# TODO: Remove column renaming from readEnvilog and create a separate function
 # TODO: Make sure these changes are considered in other packages! (S4Level2)
