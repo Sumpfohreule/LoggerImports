834fd3496901953109d235731fafd053d770600a
Lars Isenberg <isenberg.lars@gmail.com>
2021-04-05 16:31:43 +0200

    Rewrite of tinytag read function as it was not working in a package because of data.table specific operators

--- R/readTinyTag.R
+++ R/readTinyTag.R
@@ -3,27 +3,39 @@
 #' @param path A string as a path to the Tinytag file
 #' @export
 readTinyTag <- function(txt.path) {
-    tt.table <- as.data.table(read.delim(txt.path,
-            header = FALSE,
-            skip = 5))[, -1]
-    comma.col <- unlist(lapply(tt.table, function(x) TRUE %in% stringr::str_detect(x, pattern = ",")))
-    if (TRUE %in% comma.col) {
-        column <- names(comma.col)[comma.col]
-        tt.table[, (column) := as.numeric(stringr::str_replace(get(column), pattern = ",", replacement = "."))]
-    }
-    if (ncol(tt.table) == 2) {
-        setnames(tt.table, c("Datum", "RegenX"))
+    import <- readr::read_delim(
+        file = txt.path,
+        delim = "\t",
+        col_names = FALSE,
+        col_types = cols(),
+        skip = 5) %>%
+        select(-1)
+    if (ncol(import) == 2) {
+        names(import) <- c("Datum", "RegenX")
+    } else if (ncol(import) == 3) {
+        import <- import %>%
+            rename("RegenX" = "X4") %>%
+            tidyr::unite("Datum", X2|X3, sep = " ") %>%
+            mutate(Datum = lubridate::ymd_hms(Datum))
     } else {
-        num.col <- names(tt.table)[unlist(lapply(tt.table, is.numeric))]
-        tt.table <- tt.table[, .(Datum = paste(V2, V3), RegenX = get(num.col))]
+        stop("More than 3 columns found in tinytag file!")
     }
-    date.format <- MyUtilities::guessDateFormat(tt.table[1, Datum])
-    tt.table[, Datum := MyUtilities::as.POSIXctFixed(
-            Datum,
-            tz = "UTC",
-            format = date.format)]
-    tt.table[, Datum := lubridate::round_date(Datum, "5 mins")]
-    tt.table[, RegenX := as.numeric(stringr::str_match(RegenX, pattern = "^[0-9]+(?:\\.[0-9]+$)?"))]
-    data.table::setkey(tt.table, Datum)
-    return(tt.table)
+
+    import %>%
+        mutate(Datum = lubridate::round_date(Datum, "5 mins")) %>%
+        mutate(RegenX = stringr::str_replace(
+            RegenX,
+            pattern = " mm$",
+            replacement = ""
+        )) %>%
+        mutate(RegenX = stringr::str_replace(
+            RegenX,
+            pattern = ",",
+            replacement = "."
+        )) %>%
+        mutate(across(where(is.character), as.numeric)) %>%
+        data.table::as.data.table()
+
+    # Check if following pattern replacement was needed in some cases
+    # tt.table[, RegenX := as.numeric(stringr::str_match(RegenX, pattern = "^[0-9]+(?:\\.[0-9]+$)?"))]
 }
